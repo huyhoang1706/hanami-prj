f2e326e6a2acb2ddc691a01e153500177f30da7a
huyhoangg1706 <huyhoangg1706@gmail.com>
2024-11-28 23:26:45 +0700

    initialize project

---
+++ src/app/(public)/menu/page.tsx
@@ -0,0 +1,5 @@
+interface Props {}
+
+export default function MenuPage({}: Props) {
+  return <div>MenuPage</div>;
+}
---
+++ src/app/(public)/order/page.tsx
@@ -0,0 +1,7 @@
+interface Props {}
+
+export default function OrderPage({}: Props) {
+  return (
+    <div>OrderPage</div>
+  )
+}
